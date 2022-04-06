e5d8033157606f0b487343e2401bcd6021d18355
Erdem Bektaş <36085862+actasalways@users.noreply.github.com>
2022-04-06 19:43:53 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -99,48 +99,4 @@ router.post('/oturum', (req, res, next) => {
   });
 });
 
-router.get('/newcustomer', (req, res) => {
-  const customer = new Customer({
-      name:"falan kişi",
-      surname:"filan soyad",
-      tcno: "11111111111",
-      telno: "0555 555 55 55"
-  });
-
-  customer.save((err,data)=>{
-      if(err)
-      console.log(err);
-      res.json(data);
-  });
-});
-
-router.get('/newroom', (req, res) => {
-  const room = new Room({
-    no: 1,
-    dailyPrice:50,
-    renterId: "5eb803d283fa8636d038d99f",
-    howManyBeds: 1
-  });
-
-  room.save((err,data)=>{
-      if(err)
-      console.log(err);
-      res.json(data);
-  });
-});
-
-router.get('/newuser', (req, res) => {
-  const user = new User({
-      username:"q",
-      pass:"qwe",
-      isAdmin: false
-  });
-  user.save((err,data)=>{
-      if(err)
-      console.log(err);
-      res.json(data);
-  });
-});
-
-
 module.exports = router;
