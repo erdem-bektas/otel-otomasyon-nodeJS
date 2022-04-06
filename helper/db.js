acaad8cb8ebe43e95b0a6c3e6d0cfed8fbe7d36d
Erdem Bektaş <36085862+actasalways@users.noreply.github.com>
2022-04-06 19:46:08 +0300

    Update db.js

--- helper/db.js
+++ helper/db.js
@@ -6,9 +6,10 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
-
-mongoose.connect('mongodb+srv://opia:eECdLtCHQFjh5jJ2@cluster-l1u5l.mongodb.net/');
-//mongodb+srv://<username>:<password>@cluster-l1u5l.mongodb.net/test?retryWrites=true&w=majority
+  
+//mongodb connection string  should be pasted   
+mongoose.connect('<mongodb connection link>');
+  
 mongoose.connection.on('open',()=>{
   console.log('MongoDB Connected');
 });
